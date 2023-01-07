6454d608b33f3d2a2167a62b185f227082ef138a
Emka5885 <masiak-emilia@wp.pl>
2023-01-07 18:12:16 +0100

    Changes to Main Menu and Back to Main Menu

--- MainMenu/SpecialThanks.gd
+++ MainMenu/SpecialThanks.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 var madeByScene = load("res://MainMenu/MadeBy.tscn")
 
-func _on_BackButton_button_up():
+func _on_Button_button_up():
 	get_tree().change_scene(madeByScene.resource_path)
