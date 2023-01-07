75856372590d13497b9b291c642bc27dfc506234
Nartyna <mplutwska@gmail.com>
2023-01-07 14:49:55 +0100

    Add wandering to ghost npc

--- GhostNPC/GhostNPC.gd
+++ GhostNPC/GhostNPC.gd
@@ -1,41 +1,61 @@
-extends StaticBody2D
+extends KinematicBody2D
 
-var active = false
-var mouse_in = false
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+export var WANDER_TARGET_RANGE = 4
+
+enum {
+	IDLE,
+	WANDER
+}
+
+var velocity = Vector2.ZERO
+var state = IDLE
 
 onready var sprite = $Sprite
 onready var label = $Label
 onready var animationPlayer = $AnimationPlayer
+onready var wanderController = $WanderController
+onready var playerDetectionZone = $PlayerDetectionZone
 
-func _on_InteractionArea_body_entered(body):
+func _on_PlayerDetectionZone_body_entered(body):
 	if body is Player:
 		label.visible = true
 		animationPlayer.play("FadeIn")
-		active = true
 
-func _on_InteractionArea_body_exited(body):
+func _on_PlayerDetectionZone_body_exited(body):
 	if body is Player:
 		animationPlayer.play("FadeOut")
 		yield(get_tree().create_timer(1), "timeout")
 		label.visible = false
-		active = false
 
-#func _on_Pumpkin_mouse_entered():
-#	if active:
-#		sprite.material.set_shader_param("on", true)
-#		mouse_in = true
-#
-#func _on_Pumpkin_mouse_exited():
-#	if active:
-#		sprite.material.set_shader_param("on", false)
-#		mouse_in = false
-#
-#func _input(event):
-#	if active and mouse_in:
-#		if event is InputEventMouseButton:
-#			if event.button_index == BUTTON_LEFT and event.pressed:
-#				if PlayerStats.fertilizer > 0:
-#					animationPlayer.play("bounce")
-#					self.size += PlayerStats.fertilizer
-#					PlayerStats.fertilizer = 0
-#
+func _physics_process(delta):	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			if wanderController.get_time_left() == 0:
+				update_wander()
+		WANDER:
+			if wanderController.get_time_left() == 0:
+				update_wander()
+			accelerate_towards_point(wanderController.target_position, delta)
+			if global_position.distance_to(wanderController.target_position) <= WANDER_TARGET_RANGE:
+				update_wander()
+	
+	if playerDetectionZone.can_see_player():
+		state = IDLE
+	velocity = move_and_slide(velocity)
+
+func accelerate_towards_point(point, delta):
+	var direction = global_position.direction_to(point)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocity.x < 0
+
+func update_wander():
+	state = pick_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(rand_range(1, 3))
+
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
