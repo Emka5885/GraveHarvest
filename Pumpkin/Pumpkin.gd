abd64c96210de65ccb5d4040a3a108dd582fb0e0
Nartyna <mplutwska@gmail.com>
2023-01-07 15:32:13 +0100

    Add animated label to the pumpkin that indicates how much fertilizer has been added

--- Pumpkin/Pumpkin.gd
+++ Pumpkin/Pumpkin.gd
@@ -5,7 +5,10 @@ var active = false
 var mouse_in = false
 
 onready var sprite = $Sprite
-onready var animationPlayer = $AnimationPlayer
+onready var pumpkinAnimation = $PumpkinAnimation
+onready var labelAnimation = $LabelAnimation
+onready var label = $Label
+
 func _on_InteractionArea_body_entered(body):
 	if body is Player:
 		active = true
@@ -29,7 +32,9 @@ func _input(event):
 		if event is InputEventMouseButton:
 			if event.button_index == BUTTON_LEFT and event.pressed:
 				if PlayerStats.fertilizer > 0:
-					animationPlayer.play("bounce")
+					label.text = "+"+str(PlayerStats.fertilizer)
+					labelAnimation.play("TextAnim")
+					pumpkinAnimation.play("Bounce")
 					self.size += PlayerStats.fertilizer
 					PlayerStats.fertilizer = 0
 
