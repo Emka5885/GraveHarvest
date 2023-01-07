9cb8e85a0240181989cd86ec3671f4eeb19cd42d
Alaliszon121 <isabelle.prince.umh@gmail.com>
2023-01-07 22:35:39 +0100

    Cutscene Animation

--- MainMenu/Cutscene.gd
+++ MainMenu/Cutscene.gd
@@ -8,13 +8,16 @@ func _ready():
 	videoPlayer.play()
 	yield(get_tree().create_timer(0.5), "timeout")
 	videoPlayer.paused = true
-	$AnimationPlayer.play("Scale")
+	$AnimationPlayer.play("ScaleToBig")
 
-func _on_Scale_Animation_finished():
+func _on_ScaleToBig_Animation_finished():
 	videoPlayer.paused = false
 
 func _on_VideoPlayer_finished():
 	timer.start()
 
 func _on_Timer_timeout():
+	$AnimationPlayer.play("ScaleToSmall")
+
+func _on_ScaleToSmall_Animation_finished():
 	get_tree().change_scene(mainGameScene.resource_path)
