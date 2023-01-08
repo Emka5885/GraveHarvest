30577880dbd88fb2d1b4e2cad55b95827a0cbe8d
Emka5885 <masiak-emilia@wp.pl>
2023-01-08 20:19:13 +0100

    Menu Sounds

--- MainMenu/Stats.gd
+++ MainMenu/Stats.gd
@@ -2,5 +2,9 @@ extends Node2D
 
 var mainMenuScene = load("res://MainMenu/MainMenu.tscn")
 
+onready var select = $AudioStreamPlayer
+
 func _on_Button_button_up():
+	select.play()
+	yield(select, "finished")
 	get_tree().change_scene(mainMenuScene.resource_path)
