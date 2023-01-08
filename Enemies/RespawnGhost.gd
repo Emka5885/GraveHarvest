aa7776b58af25b7ccb40a6e6f2d219d17c8ee0b7
Alaliszon121 <isabelle.prince.umh@gmail.com>
2023-01-08 15:02:13 +0100

    Ghost Spawn

--- Enemies/RespawnGhost.gd
+++ Enemies/RespawnGhost.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 var ghost = load("res://Enemies/Ghost.tscn")
+onready var positions = [$Position1.position, $Position2.position, $Position3.position, $Position4.position, $Position5.position, $Position6.position, $Position7.position]
 
 onready var respawn_position
-onready var spawn_positions = []
+onready var spawn_position
+
+func _ready():
+	$Timer2.start()
 
 func _on_Ghost_respawn(start_position):
 	respawn_position = start_position
@@ -12,4 +16,11 @@ func _on_Ghost_respawn(start_position):
 func _on_Timer_timeout():
 	var instance = ghost.instance()
 	instance.set_position(respawn_position)
+	call_deferred("add_child", instance)
+
+func _on_Timer2_timeout():
+	positions.shuffle()
+	spawn_position = positions.pop_front()
+	var instance = ghost.instance()
+	instance.set_position(spawn_position)
 	call_deferred("add_child", instance)
