437cac25e3c4a752dc7af0a2b39a91ab1ae4195e
Alaliszon121 <isabelle.prince.umh@gmail.com>
2023-01-08 17:35:56 +0100

    More ghost spawners

--- Enemies/RespawnGhost.gd
+++ Enemies/RespawnGhost.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var ghost = load("res://Enemies/Ghost.tscn")
 
-onready var positions = [$Position1.position, $Position2.position, $Position3.position, $Position4.position, $Position5.position, $Position6.position, $Position7.position]
+onready var positions = [$Position1.position, $Position2.position, $Position3.position, $Position4.position, $Position5.position, $Position6.position, $Position7.position, $Position8.position, $Position9.position, $Position10.position]
 var counter := 0
 onready var respawn_position
 onready var spawn_position
@@ -22,7 +22,7 @@ func _on_Timer_timeout():
 func _on_Timer2_timeout():
 	spawn_position = positions[counter]
 	counter += 1
-	if counter == 7:
+	if counter == 9:
 		counter = 0
 	var instance = ghost.instance()
 	instance.set_position(spawn_position)
