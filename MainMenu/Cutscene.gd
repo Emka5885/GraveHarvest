b7c2b603552dd30775ecfe2ae46a4c67c5694b5b
Emka5885 <masiak-emilia@wp.pl>
2023-01-08 15:21:27 +0100

    Test cutscene

--- MainMenu/Cutscene.gd
+++ MainMenu/Cutscene.gd
@@ -3,15 +3,12 @@ extends Node2D
 var mainGameScene = load("res://World.tscn")
 onready var videoPlayer = $VideoPlayer
 onready var timer = $Timer
-onready var sprite = $Sprite
 
 func _ready():
-	sprite.visible = true
 	videoPlayer.play()
 	yield(get_tree().create_timer(0.5), "timeout")
-	videoPlayer.paused = true
-	$AnimationPlayer.play("ScaleToBig")
-	sprite.visible = false
+	#videoPlayer.paused = true
+	#$AnimationPlayer.play("ScaleToBig")
 
 func _on_ScaleToBig_Animation_finished():
 	videoPlayer.paused = false
@@ -20,7 +17,6 @@ func _on_VideoPlayer_finished():
 	timer.start()
 
 func _on_Timer_timeout():
-	print("1")
 	$AnimationPlayer.play("ScaleToSmall")
 
 func _on_ScaleToSmall_Animation_finished():
