9bd8e4afafb405b18656e1f4322aacab0a6ea65a
Alaliszon121 <isabelle.prince.umh@gmail.com>
2023-01-09 18:42:34 +0100

    Click menu sound added

--- World/Tablet.gd
+++ World/Tablet.gd
@@ -8,5 +8,5 @@ func _on_Area2D_body_entered(body):
 func _on_Out_Animation_finished():
 	$Panel.visible = false
 
-func _on_Area2D_body_exited(body):
+func _on_Area2D_body_exited(_body):
 	$AnimationPlayer.play("out")
