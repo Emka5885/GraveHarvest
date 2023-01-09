c4f03124271c6e947e317f45774439395850659c
Alaliszon121 <isabelle.prince.umh@gmail.com>
2023-01-09 17:47:07 +0100

    Tablet added

--- World/Tablet.gd
+++ World/Tablet.gd
@@ -1,17 +1,12 @@
 extends StaticBody2D
 
 func _on_Area2D_body_entered(body):
-	if body == Player:
+	if body is Player:
 		$AnimationPlayer.play("in")
 		$Panel.visible = true
-		$Timer.start()
-
-func _process(delta):
-	if Input.is_action_just_pressed("ui_select"):
-		$AnimationPlayer.play("out")
 
 func _on_Out_Animation_finished():
 	$Panel.visible = false
 
-func _on_Timer_timeout():
+func _on_Area2D_body_exited(body):
 	$AnimationPlayer.play("out")
