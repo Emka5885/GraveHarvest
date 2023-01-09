7aaf9fdbba475e776e7d59ed4cce7b348ee2e7f9
Emka5885 <masiak-emilia@wp.pl>
2023-01-09 19:48:21 +0100

    Fix Menu (joystick and keyboard)

--- MainMenu/HowToPlay.gd
+++ MainMenu/HowToPlay.gd
@@ -8,14 +8,10 @@ func _physics_process(_delta):
 	var joysticks = Input.get_connected_joypads()
 	if joysticks.size() != 0:
 		$Back2.visible = true
-		$Back1.visible = true
-		$Button.visible = false
 		if Input.is_action_just_pressed("select"):
 			_on_Button_button_up()
 	else:
 		$Back2.visible = false
-		$Back1.visible = false
-		$Button.visible = true
 
 
 func _on_Button_button_up():
