80b6c92b5d192df08747ac73119a950be3036dc8
Emka5885 <masiak-emilia@wp.pl>
2023-01-10 01:50:07 +0100

    smoother diagonal movement (joystick)

--- Player/Player.gd
+++ Player/Player.gd
@@ -47,7 +47,6 @@ func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
